7507bf93f935cfc926c697b143021bcda86f31ac
knoxknox <alexandr_y@mail.ru>
2017-08-05 01:31:42 +0300

    Add concerns

---
+++ lib/buffer/concerns/formatter.rb
@@ -0,0 +1,25 @@
+module Buffer
+  module Concerns
+    module Formatter
+      def chars
+        map(&:chr)
+      end
+
+      def str
+        map(&:chr).join
+      end
+
+      def bits
+        map { |x| "%08d" % x.to_s(2) }
+      end
+
+      def hex
+        bytes.map { |x| x.to_s(16).rjust(2, '0') }.join.upcase
+      end
+
+      def base64(strict: true)
+        strict ? Base64.strict_encode64(str) : Base64.encode64(str)
+      end
+    end
+  end
+end
---
+++ lib/buffer/concerns/traversable.rb
@@ -0,0 +1,25 @@
+module Buffer
+  module Concerns
+    module Traversable
+      def last(count = 1)
+        ByteBuffer.new(bytes.last(count))
+      end
+
+      def first(count = 1)
+        ByteBuffer.new(bytes.first(count))
+      end
+
+      def each_byte(&block)
+        ByteBuffer.new(bytes.map { |x| yield(x) })
+      end
+
+      def chunk(start, length)
+        ByteBuffer.new(bytes.slice(start - 1, length))
+      end
+
+      def each_chunk(count)
+        bytes.each_slice(count) { |x| yield ByteBuffer.new(x) }
+      end
+    end
+  end
+end
