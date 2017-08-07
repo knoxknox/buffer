8b94eeb38ca615032d442e9d88e721e29955f963
knoxknox <alexandr_y@mail.ru>
2017-08-07 22:57:48 +0300

    More tests

---
+++ test/unit/chunk_test.rb
@@ -0,0 +1,24 @@
+require_relative '../test_helper'
+
+class ChunkTest < MiniTest::Test
+
+  def setup
+    @buffer = Buffer.str('test example')
+  end
+
+  def test_chunks
+    chunks = [
+      [0x74, 0x65, 0x73],
+      [0x74, 0x20, 0x65],
+      [0x78, 0x61, 0x6d],
+      [0x70, 0x6c, 0x65]
+    ]
+
+    current_chunk = 0
+    @buffer.each_chunk(3) do |chunk|
+      current_chunk += 1
+      assert_equal(chunk.bytes, chunks[current_chunk - 1])
+    end
+  end
+
+end
